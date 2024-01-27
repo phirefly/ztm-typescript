0c8993972ad78ddbe5ee5f3c6ff3c94a0852352a
Raphael Villas <raphael.villas@gmail.com>
2024-01-27 16:44:11 -0600

    Type aliases: why they're important

--- demo/type-alias/type-alias.ts
+++ demo/type-alias/type-alias.ts
@@ -9,3 +9,21 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-aliases
+
+type personName = string; //wherever you see personName, it is a string
+
+const name: personName = "Raphy Villas";
+const anotherName: string = "Anne Villas";
+const aBadString: string = "a house"
+
+function printNameBad(name: string) {
+    console.log(`Supposed to be a name: ${name}`);
+}
+
+function printNameBetter(name: personName) {
+    console.log(`Supposed to be a name and we can tell! => ${name}`);
+}
+
+printNameBad("my foot");
+
+printNameBetter("Raphael Villas");
