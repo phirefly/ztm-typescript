031308a69a7595bfde03bdcf1abd09836ae33b23
Raphael Villas <raphael.villas@gmail.com>
2024-02-03 18:59:15 -0600

    Initial grep script

---
+++ app/grep/grep.ts
@@ -0,0 +1,41 @@
+import { readFileSync } from "fs";
+
+type SearchConfig = {
+  [key: string]: {
+    indicator: string;
+    header: string;
+  }
+};
+
+const myConfig: SearchConfig = {
+  todo: {
+    indicator: "__",
+    header: "Actions"
+  },
+  important: {
+    indicator: "**",
+    header: "Important Things"
+  },
+  question: {
+    indicator: "Q:",
+    header: "Questions"
+  },
+}
+
+const args = process.argv.slice(2);
+console.log(args);
+
+const filename = args[0];
+console.log("searching in", filename);
+const file = readFileSync(filename, "utf-8");
+
+Object.keys(myConfig).forEach((key) => {
+  const config = myConfig[key];
+  const lines = file.split("\n");
+  console.log("\n-----", config.header, "-----");
+  lines.forEach((line, index) => {
+    if (line.includes(config.indicator)) {
+      console.log(line);
+    }
+  });
+});
