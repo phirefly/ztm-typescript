3d7114cb4ce0db248dd94135c34195b44a9a07f2
Raphael Villas <raphael.villas@gmail.com>
2024-02-12 18:51:09 -0600

    Added exercises for modules

--- demo/modules/modules.ts
+++ demo/modules/modules.ts
@@ -1,5 +1,8 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
+import { add, pi, Int as MyInteger } from "./math";
+import { Point as MyPoint } from "./coords";
+
 
 // ES modules provide a way to organize code into separate files that can be
 // imported and used in other files.To use an ES module, the the `import`
@@ -7,3 +10,10 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/modules.html
+
+
+const three: MyInteger = 3;
+const four: MyInteger = 4;
+
+const result = add(three, four);
+assert.equal(result, 7);
