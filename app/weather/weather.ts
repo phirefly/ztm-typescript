8ae5836f65a40111da805a6e8021600901321709
Raphael Villas <raphael.villas@gmail.com>
2024-03-03 21:56:00 -0600

    WIP instructions

--- app/weather/weather.ts
+++ app/weather/weather.ts
@@ -1,6 +1,20 @@
+const GEOCODE_API_URL = 'https://geocode.maps.co/search';
+
 async function main(): Promise<number> {
   // > pnpm run weather LOCATION
   console.log(process.argv)
+
+  // handle correct amount of args
+  if (process.argv.length !== 3) {
+    console.error("Format needed: pnpm weather LOCATION")
+    return 1; //returning 1 means an error occurred. 0 means success.
+  }
+
+  const location = argv[2];
+  // 1. Use location api to get lat/long value for this location
+  // 2. Fetch weather data
+  // 3. Display weather data
+
   return await Promise.resolve(0);
 
 }
