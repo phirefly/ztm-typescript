11c02cd52b5b6f5a1fd59286c93c5613f8f4390d
Raphael Villas <raphael.villas@gmail.com>
2024-03-18 18:35:33 -0500

    Added example of const assertion

--- demo/const-assert/const-assert.ts
+++ demo/const-assert/const-assert.ts
@@ -9,3 +9,59 @@
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
+
+
+//Start with a union tpe
+import {Departments} from "../dts2/mylib";
+import {exec} from "node:child_process";
+
+type Rgb = "red" | "green" | "blue"
+//=> allows you to do this...
+const red: Rgb = "red";
+
+// Creating a new union type by...
+// using 'as const' and
+// using 'typeof'
+
+// as const tells typescript that it, in this case the array, isn't going to change. It's readonly data
+{
+    const Color = ["red", "green", "blue"] as const;
+    type Color = (typeof Color)[number];
+    //=> allows you to do...
+    const blue: Color = "blue";
+
+
+    // Why do we need const assertion?
+    //=> You can iterate through each one like this...
+    // It's also unchanging, immutable
+    // You can't do that with a normal union type
+    for (const c of Color) {
+        console.log(c);
+    }
+}
+
+// Another example:
+{
+    const Department = {
+        Executive: "top floor",
+        Sales: "middle floor",
+        Warehouse: "bottom floor",
+
+    } as const;
+    type Department = (typeof Department)[keyof typeof Department];
+
+    let k: keyof typeof Department //--> I don't get this
+    for (k in Department) {
+        console.log(`key=${k}, floor=${Department[k]}`)
+    }
+
+    // Example: getting the executive department
+    const execDept: Department = Department.Executive;
+    console.log(`executive department location: ${execDept}`);
+
+}
+
+
+
+
+
